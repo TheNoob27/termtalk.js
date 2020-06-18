03f79a5c6f0b4077f9d1cffbe76cd7e3976f3341
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-18 22:33:24 +0100

    uh this..? 💀

--- src/managers/ServerManager.js
+++ src/managers/ServerManager.js
@@ -1,1 +1,10 @@
+const BaseManager = require("./BaseManager.js")
+const Server = require("../structures/Server.js")
 
+class ServerManager extends BaseManager {
+  constructor(client) {
+    super(client, Server)
+  }
+}
+
+module.exports = ServerManager
