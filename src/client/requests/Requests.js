ab90d2264068ce4676355e88f78306b638ebe781
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-18 20:47:45 +0100

    create the requests structure
    bruh idk where i'm going with this

--- src/client/requests/Requests.js
+++ src/client/requests/Requests.js
@@ -1,3 +1,13 @@
+const createRoute = require("./API.js")
+
 class Requests {
-	
+  constructor(client) {
+    this.client = client
+  }
+  
+  get api() {
+    return createRoute(this)
+  }
 }
+
+module.exports = Requests
