cb33d1424febe9bffc9271749cf7bb104cc084a4
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-18 20:56:58 +0100

    draft

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,6 +1,25 @@
+const Requests = require("./requests/Requests.js")
 
 class Client {
-  constructor() {
-		
-	}
+  constructor(options) {
+    this.requests = new Requests(this)
+    this.options = this.parseOptions(options)
+  }
+  
+  get rest() {
+    return this.requests
+  }
+  
+  parseOptions(options) {
+    const defaultOptions = {
+      ip: "localhost", 
+      port: 3000,
+      token: null
+    }
+    
+    if (!options || typeof options !== "object") return defaultOptions
+    
+    if ("ip" in options && typeof options.ip !== "string") throw new TypeError("The IP must be a string.")
+    if ("port" in options && (typeof options.port !== "number" || isNaN(options.port)) throw new TypeError("The port must be a number.")
+  }
 }
