6379301337343eaedcd36bafa70ab5136d836ace
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-18 23:26:56 +0100

    base for server class

--- src/structures/Server.js
+++ src/structures/Server.js
@@ -1,1 +1,32 @@
+const Base = require("./Base.js")
+const MemberManager = require("../managers/MemberManager.js")
+const ChannelManager = require("../managers/ChannelManager.js")
 
+class Server extends Base {
+  constructor(client, data) {
+    super(client)
+    
+    this.members = new MemberManager(this)
+    this.channels = new ChannelManager(this)
+    
+    if (data) this._patch(data) // lol probably not gonna ever be any data
+  }
+  
+  _patch(data) {
+    if (data.token) Object.defineProperty(this, "token", { value: data.token, writable: true })
+    
+    if (data.channels) {
+      this.channels.cache.clear()
+      for (const channel of data.channels) this.channels.add(channel)
+    }
+    
+    if (data.members) {
+      this.members.cache.clear()
+      for (const member of data.members) this.members.add(member)
+    }
+  }
+  
+  fetch() {
+    // gonna do client requests soon
+  }
+}
