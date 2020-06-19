05152bb7c93a2dd2b5e7d099520b3190088b5088
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-19 22:31:23 +0100

    limited cache size

--- src/managers/MessageManager.js
+++ src/managers/MessageManager.js
@@ -1,9 +1,10 @@
 const BaseManager = require("./BaseManager.js")
 const Message = require("../structures/Message.js")
+const LC = require("../util/LimitedCollection.js")
 
 class MessageManager extends BaseManager {
   constructor(channel) {
-    super(channel.client, Message)
+    super(channel.client, Message, null, LC, channel.client.options.messageCacheSize || 30)
     
     this.channel = channel
   }
