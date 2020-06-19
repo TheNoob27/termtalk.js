f23c8b80d98e6b38b15b23a43b7e5453be7dd31c
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-19 18:10:21 +0100

    channel

--- src/structures/Channel.js
+++ src/structures/Channel.js
@@ -1,5 +1,33 @@
 const Base = require("./Base.js")
+const Messages = require("../managers/MessageManager.js")
+const { Error } = require("../errors")
 
-class Channel extends Base {}
+class Channel extends Base {
+  constructor(client, data, server) {
+    super(client)
+    
+    this.server = server
+    this.messages = new Messages(this)
+    if (data) this._patch(data)
+  }
+  
+  _patch(data) {
+    this.name = data.name || "General"
+  }
+  
+  send(msg) {
+    if (typeof msg !== "string") return Promise.reject(new Error("MESSAGE_TYPE"))
+    
+    return this
+      .server.api
+      .channels(this.id)
+      .messages
+      .post({
+        ...this.client.user.json,
+        msg
+      })
+      .then(message => this.messages.add(message, { extras: [this] }))
+  }
+}
 
 module.exports = Channel
