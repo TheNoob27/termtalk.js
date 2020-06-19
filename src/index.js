647e6bf577c87052272089409a7d9f79c4f25ced
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-19 20:00:24 +0100

    update with all the progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = {
-  Client: require("./client/Client.js")
+  Client: require("./client/Client.js"),
+  
+  Collection: require("./util/Collection.js"),
+  LimitedCollection: require("./util/LimitedCollection.js"),
+  
+  Base: require("./structures/Base.js"),
+	Channel: require("./structures/Channel.js"),
+	ClientUser: require("ClientUser.js"),
+	Member: require("./structures/Member.js"),
+	Server: require("./structures/Server.js"),
+	User: require("./structures/User.js"),
+  
+  BaseManager: require("./managers/BaseManager.js"),
+  ChannelManager: require("./managers/ChannelManager.js"),
+  MemberManager: require("./managers/MemberManager.js"),
+  MessageManager: require("./managers/MessageManager.js"),
+  ServerManager: require("./managers/ServerManager.js"),
+  UserManager: require("./managers/UserManager.js"),
 }
