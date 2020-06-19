80765403ca692e98259e9ed8075f08deb87b15b2
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-19 06:56:03 +0100

    create bot

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,17 +1,22 @@
 const RequestsManager = require("./requests/RequestsManager.js")
-const { TypeError } = require("../errors")
+const { Error, TypeError, RangeError } = require("../errors")
 const Server = require("../structures/Server.js")
 const ServerManager = require("../managers/ServerManager.js")
+const UserManager = require("../managers/UserManager.js")
 
 class Client {
   constructor(options) {
+    this.http = require("http") // default http
     this.options = this.parseOptions(options)
+    
     this.requests = new RequestsManager(this)
     this.servers = new ServerManager(this, [
-      this.options.ip instanceof Array ? 
-      this.options.ip.map((ip, i) => ({ ip, port: this.options.port[i] })) :
-      { ip: this.options.ip, port: this.options.port }
+      this.options.ip.map((ip, i) => ({ ip, port: this.options.port[i] }))
     ])
+  }
+  
+  get api() {
+    return this.requests.api
   }
   
   get rest() {
@@ -34,21 +39,52 @@ class Client {
     const defaultOptions = {
       ip: "localhost", // lol ik you cant but thats their fault
       port: 3000 // random port
+      // idk more options soon
     },
     options = {}
     
     if (!data || typeof data !== "object") return defaultOptions
     
-    if ("ip" in data && (typeof data.ip !== "string" && !(data.ip instanceof Array) || data.ip.length <= 1)) throw new TypeError("OPTIONS_INVALID")
+    if ("ip" in data && (typeof data.ip !== "string" && !(data.ip instanceof Array) || !data.ip.length)) throw new TypeError("OPTIONS_INVALID")
     if ("port" in data && (typeof data.port !== "number" || isNaN(options.port))) throw new TypeError("OPTIONS_INVALID")
-    // 1 is not array and 2 is array
+    // 1 is not array and other is array
     if ([Array.isArray(data.port), Array.isArray(data.ip)].reduce((prev, a) => a === prev ? a : null) === null) throw new TypeError("OPTIONS_INVALID")
+    if (Array.isArray(data.ip) ? data.ip.length !== data.port.length : false) throw new RangeError("OPTIONS_LENGTH")
+    
+    if (!Array.isArray(data.ip)) Object.assign(data, {
+      ip: [data.ip],
+      port: [data.port]
+    })
     
     for (const k of Object.keys(defaultOptions)) {
       options[k] = data[k] || defaultOptions[k]
     }
     
     return options
+  }
+  
+  create({ id, username, tag, uid: _, ownerID, ownerUid, ownerPassword, ip, port } = {}, options) {
+    if (_ && !id) id = _
+    if (ownerUid && !ownerID) ownerID = ownerUid
+    const data = [id, username, tag, ownerID, ownerPassword, ip, port]
+    
+    if (data.some(i => !i)) throw new Error("BOT_CREATE_INFO")
+    if (data.slice(0, -1).some(i => typeof i !== "string") || typeof port !== "number") throw new TypeError("BOT_CREATE_TYPE")
+    
+    const server = this.servers.add({ ip, port }, { cache: false })
+    
+    return this.api.server(server).bots.create.get({
+      ownerUid: ownerID,
+      ownerPassword: ownerPassword,
+      uid: id,
+      username,
+      tag
+    }).then(data => {
+      if (!this.options.ip || typeof this.options.ip === "string") this.options.ip = ip
+      if (!this.options.port || typeof this.options.port === "number") this.options.port = port
+      this.servers.add({ ip, port})
+      return data
+    })
   }
 }
 
