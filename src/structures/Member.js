dd65d40ba2f2b4971c2c1e65feb04b79334ff34f
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-19 17:42:56 +0100

    member class i guess

--- src/structures/Member.js
+++ src/structures/Member.js
@@ -1,6 +1,25 @@
 const Base = require("./Base.js")
 const User = require("./User.js")
 
-class Member extends Base {}
+class Member extends Base {
+  constructor(client, data, server) {
+    super(client)
+    this.server = server
+    
+    if (data) this._patch(data)
+  }
+  
+  _patch(data) {
+    this.admin = Boolean(data.admin)
+    this.user = this.client.users.add(data)
+    // members are bare aside user data
+  }
+  
+  send(msg) {
+    if (typeof msg !== "string") return Promise.reject(new Error("MESSAGE_TYPE"))
+    
+    return this.server.api.members[this.id].messages.post({ ...this.user.json, msg })
+  }
+}
 
 module.exports = Member
