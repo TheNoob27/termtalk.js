30c31fabfdaff51e1ce735b14e0f1826d0eee4c0
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-19 05:30:59 +0100

    query stringifier

--- src/client/requests/Requests.js
+++ src/client/requests/Requests.js
@@ -13,9 +13,31 @@ class RequestsManager {
     return this.request
   }
 
-  request(method, route, options) {
-    method = method.toUpperCase()
+  request(data, options) {
+    Object.assign(data, {
+      headers: {
+        Authorization: `Bot ${this.client.token}`,
+       "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+  }
+  
+  _stringifyQuery(json) {
+    if (!json || typeof json !== "object") return ""
+    let str = "?"
+    let f = s => encodeURIComponent(typeof s === "number" && isFinite(s) ? s : typeof s !== "number" ? s : "")
     
+    for (const [key, val] of Object.entries(json)) {
+      let prop = f(key)
+      if (!prop) continue;
+      
+      let value = !Array.isArray(val) ? f(val) : val.map(f).filter(v => v !== "").join(`&${prop}=`)
+      if (!value) continue;
+      
+      str += `${prop}=${value}`
+    }
+    
+    return str === "?" ? "" : str
   }
 }
 
