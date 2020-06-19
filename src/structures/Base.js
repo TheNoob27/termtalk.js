3646a220ddcd2f74aeb2424dd0f9b96a3b405e62
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-19 17:00:45 +0100

    base

--- src/structures/Base.js
+++ src/structures/Base.js
@@ -1,3 +1,27 @@
-class Base {}
+const Collection = require("../util/Collection.js")
+
+class Base {
+  constructor(client) {
+    Object.defineProperty(this, client, { value: client })
+  }
+  
+  _patch() {}
+  
+  valueOf() {
+    return this.id
+  }
+  
+  toJSON(ignore) {
+    if (!(ignore instanceof Array)) ignore = []
+    
+    const keys = Object.keys(this).filter(k => !k.startsWith('_') && !ignore.includes(k))
+    const json = {}
+    
+    // long code big boi code
+    for (const key of keys) json[key] = this[key] instanceof Collection ? this[key].keys() : this[key] && typeof this[key] === "object" && typeof this[key].valueOf === "function" ? this[key].valueOf() : this[key]
+    
+    return json
+  }
+}
 
 module.exports = Base
