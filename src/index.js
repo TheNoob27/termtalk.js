ef7ae8aa6ed791bde0a6c7062dbcb048e2dfcf43
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-06-22 02:24:08 +0100

    version

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 module.exports = {
+  version: require("../package.json").version,
+  
   Client: require("./client/Client.js"),
   
   Collection: require("./util/Collection.js"),
   LimitedCollection: require("./util/LimitedCollection.js"),
   
   Base: require("./structures/Base.js"),
-	Channel: require("./structures/Channel.js"),
+  Channel: require("./structures/Channel.js"),
   Member: require("./structures/Member.js"),
   MessageMentions: require("./structures/MessageMentions.js"),
-	Server: require("./structures/Server.js"),
+  Server: require("./structures/Server.js"),
   
   BaseManager: require("./managers/BaseManager.js"),
   ChannelManager: require("./managers/ChannelManager.js"),
